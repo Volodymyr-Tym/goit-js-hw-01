c1c8f54f39958eabb7bd2ac56bff8eac248a6006
Vova Tym <v.tymoshyshyn@gmail.com>
2024-06-03 20:18:07 +0100

    Add skripts to html file

--- js/task-1.js
+++ js/task-1.js
@@ -4,7 +4,10 @@ function makeTransaction(quantity, pricePerDroid) {
     `You ordered ${quantity} droids worth ${totalPrice} credits!`
   );
 }
+console.log("Task 1");
 
 console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
 console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
 console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
+
+console.log("");
--- js/task-3.js
+++ js/task-3.js
@@ -11,6 +11,7 @@ function getElementWidth(content, padding, border) {
 
   //   return a + b * 2 + c * 2;
 }
+console.log("Task 3");
 
 console.log(getElementWidth("50px", "8px", "4px")); // 74
 console.log(getElementWidth("60px", "12px", "8.5px")); // 101
